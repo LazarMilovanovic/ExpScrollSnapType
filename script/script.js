2b019f16ff8d53cc8d0a4cc976194d05b4ae4436
Lazar Milovanovic <milazar002@gmail.com>
2026-03-05 16:46:55 +0100

    Test

--- script/script.js
+++ script/script.js
@@ -1,19 +1,32 @@
 const sections = document.querySelectorAll("section");
-const navLinks = document.querySelectorAll("nav a");
+const navLinks = document.querySelectorAll(".nav-link");
 const githubLogo = document.getElementById("github");
 const gitHubCarouselLogo = document.getElementById("gitHubCarouselImg");
+const changeThemeBtn = document.getElementById("themeToggle");
+const typingEl = document.getElementById("typing-text");
 
-const btn = document.getElementById("themeToggle");
+const isDark = JSON.parse(localStorage.getItem("darkMode"));
 
-btn.addEventListener("click", () => {
+if (isDark) {
+  document.body.classList.add("dark");
+  changeThemeBtn.textContent = "☀️";
+  githubLogo.src = "images/logos/github-light.svg";
+  gitHubCarouselLogo.src = "images/logos/github-light.svg";
+}
+
+changeThemeBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 
-  if (document.body.classList.contains("dark")) {
-    btn.textContent = "☀️";
+  const darkModeActive = document.body.classList.contains("dark");
+
+  localStorage.setItem("darkMode", JSON.stringify(darkModeActive));
+
+  if (darkModeActive) {
+    changeThemeBtn.textContent = "☀️";
     githubLogo.src = "images/logos/github-light.svg";
     gitHubCarouselLogo.src = "images/logos/github-light.svg";
   } else {
-    btn.textContent = "🌙";
+    changeThemeBtn.textContent = "🌙";
     githubLogo.src = "images/logos/github-dark.svg";
     gitHubCarouselLogo.src = "images/logos/github-dark.svg";
   }
@@ -35,3 +48,14 @@ function setActiveLink() {
 window.addEventListener("scroll", setActiveLink);
 
 setActiveLink();
+
+const text = "I'm Frontend Developer";
+let i = 0;
+function type() {
+  if (i < text.length) {
+    typingEl.textContent += text[i];
+    i++;
+    setTimeout(type, 80);
+  }
+}
+type();
